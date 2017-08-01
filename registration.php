92402e6f5ad74804474b0a58ce6723ec0a03d392
Seth Daugherty <sdaugherty@human-element.com>
2017-08-01 12:50:48 -0400

    Add docblock and composer package author

--- registration.php
+++ registration.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Human Element Inc.
+ *
+ * @package HumanElement_EmailLogoFix
+ * @copyright Copyright (c) 2017 Human Element Inc. (https://www.human-element.com)
+ */
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
